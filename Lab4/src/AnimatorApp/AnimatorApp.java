48304ffdd7038895b810604417b0661d403651a1
Jakub Dorda <jak_dor@wp.pl>
2017-11-23 14:45:15 +0100

    add Lab4 - swing animations

---
+++ Lab4/src/AnimatorApp/AnimatorApp.java
@@ -0,0 +1,106 @@
+package AnimatorApp;
+
+import java.awt.EventQueue;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.JButton;
+import java.awt.Dimension;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+
+public class AnimatorApp extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					final AnimatorApp frame = new AnimatorApp();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 * @param delay 
+	 */
+	public AnimatorApp() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+		int ww = 450, wh = 300;
+		setBounds((screen.width-ww)/2, (screen.height-wh)/2, ww, wh);
+		contentPane = new JPanel();
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		AnimPanel kanwa = new AnimPanel();
+		kanwa.setBounds(10, 11, 422, 219);
+		contentPane.add(kanwa);
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				kanwa.initialize();
+			}
+		});
+
+		JButton btnAdd = new JButton("Add");
+		btnAdd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				kanwa.addFig();
+			}
+		});
+		btnAdd.setBounds(10, getHeight() -30, 80, 23);
+		contentPane.add(btnAdd);
+		
+		JButton btnAnimate = new JButton("Animate");
+		btnAnimate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				kanwa.animate();
+			}
+		});
+		btnAnimate.setBounds(100, getHeight()-30, 80, 23);
+		contentPane.add(btnAnimate);
+
+		this.addComponentListener(new ComponentListener() {
+			@Override
+			public void componentShown(ComponentEvent componentEvent) {
+
+			}
+
+			@Override
+			public void componentHidden(ComponentEvent componentEvent) {
+
+			}
+
+			@Override
+			public void componentMoved(ComponentEvent componentEvent) {
+
+			}
+
+			public void componentResized(ComponentEvent e) {
+				kanwa.changeSize(getWidth()-45, getHeight()-45);
+				btnAdd.setBounds(10, getHeight() -20, 80, 23);
+				btnAnimate.setBounds(100, getHeight()-20, 80, 23);
+			}
+		});
+	}
+
+}
